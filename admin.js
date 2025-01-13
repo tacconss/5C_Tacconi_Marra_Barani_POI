6594b7472cf48189b1bdd732fd6c5f1b458f55c8
Gibbo Marra <marragabriel@itis-molinari.eu>
2025-01-13 11:45:08 +0100

    pulsanti di  rimozione inseriti in una funzione

--- admin.js
+++ admin.js
@@ -10,12 +10,17 @@ const opera9=document.querySelector("#opera9");
 const opera10=document.querySelector("#opera10");
 let array1=[opera1,opera2,opera3,opera4,opera5,opera6,opera7,opera8,opera9,opera10];
 
-for(let i=0;i<array1.length;i++){
-    //console.log(arrayOpereStatic[i]);
-    array1[i].innerHTML+=`<button id=bottone${i+1}> - </button>`;
-    //console.log(array1[i]);
+
+//aggiunta dei pulsanti di rimozione per ogni riga
+const aggiungiPulsantiRimozione=()=>{
+    for(let i=0;i<array1.length;i++){
+        //console.log(arrayOpereStatic[i]);
+        array1[i].innerHTML+=`<button id=bottone${i+1}> - </button>`;
+        //console.log(array1[i]);
+    }
 }
 
+aggiungiPulsantiRimozione();
 
 
 
