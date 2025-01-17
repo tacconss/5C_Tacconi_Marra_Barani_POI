3786a77c60b390641e3e28204f1138861007733c
Gibbo Marra <marragabriel@itis-molinari.eu>
2025-01-17 11:13:57 +0100

    non sono nemmeno cosa sto facendo, sono stanco

--- admin.js
+++ admin.js
@@ -1,4 +1,8 @@
 const opere = document.querySelectorAll(".opera"); //prende tutte le opere in automatico (14/01/2024)
+const aggiungiRiga=document.querySelector("#aggiungiRiga");//bottone quando viene premuto aggiunge la riga (formAdmin)
+
+
+
 //aggiunta dei pulsanti di rimozione per ogni riga
 const aggiungiPulsantiRimozione=()=>{
     for(let i=0;i<opere.length;i++){
@@ -16,24 +20,31 @@ const rimuoviRiga=()=>{
     }
 }
 
-/*const rimuoviRiga=()=>{
-    let contEsterno=1; //permette di tenere il conto del numero del bottone
-    for(let i=0;i<array1.length;i++){
-        let array1= document.querySelector("")
-        let bottone=document.querySelector(`#bottone${i+1}`);
-        bottone.onclick=()=>{
-            console.log("ciao"+(i+1));//funziona, adesso bisogna dire che viene tolta la riga
-            //va presa la riga e va nascosta
-            array1[i].splice(i,i);
-            console.log(array1);
-            
-        }
-        contEsterno++;
+//per l'aggiunta di opere dentro la formadmin
+    let template=`<tr><td>&OPERA</td><td>&LUOGO</td><td>&DATA</td><td>&DESCRIZIONE</td></tr>`;
+    
+    aggiungiRiga.onclick=()=>{
+        /*
+        const a=document.querySelector("#idInserimentoOpera");
+        const b=document.querySelector("#idInserimentoData");
+        const c=document.querySelector("#idInserimentoLuogo");
+        const d=document.querySelector("#idInserimentoDescrizione");
+        template=template.replace("&OPERA",a.nodeValue).replace("&DATA",b.nodeValue).replace("&LUOGO",c.nodeValue).replace("&DESCRIZIONE",d.nodeValue);
+        console.log(template);
+        opere.innerHTML+=template;
+        */
+        const a=document.querySelector("#idInserimentoOpera");
+        const b=document.querySelector("#idInserimentoData");
+        const c=document.querySelector("#idInserimentoLuogo");
+        const d=document.querySelector("#idInserimentoDescrizione");
+       
     }
-}
-*/
+
+
+
 aggiungiPulsantiRimozione();
 rimuoviRiga();
+aggiungiOpera();
 
 //``
 
