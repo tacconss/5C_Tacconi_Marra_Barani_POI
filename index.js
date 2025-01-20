c8da2214e4205e9746195c4599c796442b4861d3
Tacconi <tacconisimone@itis-molinari.eu>
2025-01-20 16:44:09 +0100

    modifica index

--- index.js
+++ index.js
@@ -1,3 +1,10 @@
+import { generateFetchComponent } from "./fetch.js";
 import { createNavigator } from "./navigator.js";
 
 const navigator = createNavigator(document.querySelector("#container"));
+
+const fetchIndex = generateFetchComponent();
+
+fetchIndex.setData("prova", "ciao").then(() => {
+    fetchIndex.getData("prova").then(console.log).catch(console.error);
+});
